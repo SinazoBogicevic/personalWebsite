a51ee57a208987b9513f0ce021d4499a017c6d3c
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-08-05 15:45:51 +0200

    configure express

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 //app.use("/", routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/static", express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
