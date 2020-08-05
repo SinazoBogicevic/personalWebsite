3c99fe2b3f0d1eafe0f516f2f7251e30f4112309
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-08-05 19:32:39 +0200

    configure server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 //app.use("/", routes);
 
-app.get("/api/get", (req, res) => {
+app.get("/", (req, res) => {
   res.send(req.body);
 });
 
