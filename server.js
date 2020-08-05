d63878430770ab89704b52b7414139794b627e50
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-08-05 15:00:54 +0200

    configure routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const cors = require("cors");
-//const path = require("path");
+const path = require("path");
 
 //const routes = require("./routes");
 
@@ -13,7 +13,7 @@ app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/", routes);
+//app.use("/", routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
