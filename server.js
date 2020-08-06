5cdaa7648ae42eb37c52a250db0fd7a0a6cf40d1
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-08-06 14:23:01 +0200

    commit

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 //app.use("/", routes);
+app.use("/static", express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
