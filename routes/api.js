da304aed04c51edde2ffad877e1810a39d662e0f
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-08-07 12:41:23 +0200

    add routes folder

---
+++ routes/api.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+router.post("/api/mail", (req, res) => {
+  let data = req.body;
+  res.send(data);
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  let mailOptions = {
+    replyTo: `${data.mail}`,
+    to: "snoqhamza@yahoo.com",
+    subject: `${data.mail}`,
+    text: `${data.message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, data) => {
+    if (error) {
+      console.log("error occured", error);
+    } else {
+      console.log("email sent");
+    }
+    smtpTransport.close();
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,17 +7,13 @@ const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const path = require("path");
 
-//const routes = require("./routes");
+const routes = require("./routes/api");
 
 app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-//app.use("/", routes);
-
-app.get("/api", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/", routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
@@ -26,37 +22,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.post("/api/mail", (req, res) => {
-  let data = req.body;
-  res.send(data);
-
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
-
-  let mailOptions = {
-    replyTo: `${data.mail}`,
-    to: "snoqhamza@yahoo.com",
-    subject: `${data.mail}`,
-    text: `${data.message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, data) => {
-    if (error) {
-      console.log("error occured", error);
-    } else {
-      console.log("email sent");
-    }
-    smtpTransport.close();
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`server listening at port 3001`);
