15b308933960e79d3ee07596050cc1f0364f4a99
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-08-10 12:13:19 +0200

    build folder

---
+++ client/build/precache-manifest.ce4fb393051363a53a1b11f40cabd4e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070ca7d94868b606c993bfd799f5c699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9bdb5d6837e519ac259",
+    "url": "/static/css/main.2915c734.chunk.css"
+  },
+  {
+    "revision": "26991099e51bc5b9ac24",
+    "url": "/static/js/2.d64c4b93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d64c4b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9bdb5d6837e519ac259",
+    "url": "/static/js/main.f703fc59.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "a065afe4dc61a1801159c915c2b2fed3",
+    "url": "/static/media/SinazoB.a065afe4.jpg"
+  }
+]);
--- client/src/components/landing/About.js
+++ client/src/components/landing/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import SinazoB from "../SinazoB";
+import SinazoB from "../SinazoB.jpg";
 
 export default function About() {
   return (
